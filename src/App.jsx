677b6d4bacfa725f0bc67ab1b68a7355422723d7
Oleksii Pysarenko <pppisar@Oleksiis-MacBook-Air.local>
2024-08-24 10:15:34 +0200

    Components, Props, PropTypes and DefaultProps

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import Button from "./Button";
+import Student from "./Student";
 
 function App() {
   return (
     <>
-      <Button />
+      <Student name="Sponge Bob" age={30} isStudent={true}/>
+      <Student name="Patrick" age={42} isStudent={false}/>
+      <Student name="Squidward" age={50} isStudent={false}/>
+      <Student name="Sandy" age={27} isStudent={true}/>
+      <Student name="Larry"/>
     </>
   );
 }
